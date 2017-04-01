44cd2c46ba18f19a1ab4ef00e7e1aa52bfa9a0bb
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-04-01 15:54:25 -0300

    log error

--- src/modules/pornhub.js
+++ src/modules/pornhub.js
@@ -14,6 +14,7 @@ const Pornhub = {
           resolve(this.video.parse(response.data));
         })
         .catch((error) => {
+          console.log(error);
           reject(`No results for search related to ${relation} in page ${page}`);
         });
     });
