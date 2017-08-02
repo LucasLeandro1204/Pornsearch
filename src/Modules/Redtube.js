478d14f5875b92d0d0ed33bbe1b0bdeb9bd7dea9
Lucas Leandro <lucasleandro1204@gmail.com>
2017-08-01 21:32:06 -0300

    redtube

---
+++ src/Modules/Redtube.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let AbstractModule = require('../Core/AbstractModule');
+
+class Redtube extends AbstractModule {
+  get name () {
+    return 'redtube';
+  }
+
+  get firstpage () {
+    return 1;
+  }
+
+  videoUrl (page) {
+    return `https://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&search=${this.query}&thumbsize=big&page=${page || this.firstpage}`;
+  }
+
+  videoParser ($, { videos }) {
+    return videos.map(({ video }) => {
+      return {
+        title: video.title,
+        url: video.url,
+        duration: video.duration,
+        thumb: video.default_thumb,
+      };
+    });
+  }
+};
+
+module.exports = Redtube;
