ea6aa4f0b5923729f8e2d7b5804cb34817f11da9
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-01-07 03:30:41 -0200

    order

--- src/Core/Modules.js
+++ src/Core/Modules.js
@@ -1,6 +1,6 @@
+import sex from '@/Modules/Sex';
 import pornhub from '@/Modules/Pornhub';
 import redtube from '@/Modules/Redtube';
-import sex from '@/Modules/Sex';
 import xvideos from '@/Modules/Xvideos';
 
 export default {
